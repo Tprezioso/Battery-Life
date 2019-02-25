37a4b56c4adcc209cd22a6748fdbfdae1723cbc7
Tprezioso <tommyprezioso@gmail.com>
2019-02-25 13:09:39 -0500

    first commit

---
+++ Battery Life WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Battery Life WatchKit Extension
+//
+//  Created by Thomas Prezioso on 2/25/19.
+//  Copyright © 2019 Thomas Prezioso. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
